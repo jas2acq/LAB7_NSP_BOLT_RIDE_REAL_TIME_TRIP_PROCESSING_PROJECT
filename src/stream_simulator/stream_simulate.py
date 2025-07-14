a7a79af918d0360ab8f2db6d305e5a61a55c2376
jas2acq <jason.acquah@amalitechtraining.org>
2025-07-14 02:09:09 +0000

    Defined constants for logging and file paths

--- src/stream_simulator/stream_simulate.py
+++ src/stream_simulator/stream_simulate.py
@@ -19,3 +19,9 @@ AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_REGION = os.getenv('AWS_REGION')
 KINESIS_STREAM_NAME = os.getenv('KINESIS_STREAM_NAME')
+
+LOG_DIR = "logs"
+LOG_FILE = "streaming.log"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TRIP_START_CSV = os.path.join(BASE_DIR, "data", "trip_start.csv")
+TRIP_END_CSV = os.path.join(BASE_DIR, "data", "trip_end.csv")
